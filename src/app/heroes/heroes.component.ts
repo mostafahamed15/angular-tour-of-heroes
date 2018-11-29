0f5353e555770315b3ddee9446ce1f7b1413d646
mostafa Hamed <mostafa.hamed@esmaar.net>
2018-11-29 18:16:20 +0200

    Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {Heroes} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,19 +9,16 @@ import {Heroes} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = Heroes;
-  selectedHero: Hero;
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  }
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
+  heroes: Hero[];
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
   
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
